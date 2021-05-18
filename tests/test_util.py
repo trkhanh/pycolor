6ce1dfc081111230a4abc6b5c0bd4460cb36ef70
Tran Khanh <tran_khanh@outlook.com>
2021-05-18 15:31:26 +0700

    implement test for utils

--- tests/test_util.py
+++ tests/test_util.py
@@ -37,3 +37,56 @@ class TestUtil(unittest.TestCase):
         utils.save_file("Hello, world", tmp_file)
         result = os.path.isfile(tmp_file)
         self.assertTrue(result)
+
+    def test_save_file_json(self):
+        """> Save colors to a file."""
+        tmp_file = "/tmp/test_file.json"
+        utils.save_file_json(COLORS, tmp_file)
+        result = os.path.isfile(tmp_file)
+        self.assertTrue(result)
+
+    def test_create_dir(self):
+        """> Create a directory."""
+        tmp_dir = "/tmp/test_dir"
+        utils.create_dir(tmp_dir)
+        self.assertTrue(os.path.isdir(tmp_dir))
+        os.rmdir(tmp_dir)
+
+    def test_hex_to_rgb_black(self):
+        """> Convert #000000 to RGB."""
+        result = utils.hex_to_rgb("#000000")
+        self.assertEqual(result, (0, 0, 0))
+
+    def test_hex_to_rgb_white(self):
+        """> Convert #ffffff to RGB."""
+        result = utils.hex_to_rgb("#ffffff")
+        self.assertEqual(result, (255, 255, 255))
+
+    def test_hex_to_rgb_rand(self):
+        """> Convert #98aec2 to RGB."""
+        result = utils.hex_to_rgb("#98aec2")
+        self.assertEqual(result, (152, 174, 194))
+
+    def test_hex_to_xrgba(self):
+        """> Convert #98aec2 to XRGBA."""
+        result = utils.hex_to_xrgba("#98aec2")
+        self.assertEqual(result, "98/ae/c2/ff")
+
+    def test_rgb_to_hex(self):
+        """> Convert 152,174,194 to HEX."""
+        result = utils.rgb_to_hex((152, 174, 194))
+        self.assertEqual(result, "#98aec2")
+
+    def test_darken_color(self):
+        """> Darken #ffffff by 0.25."""
+        result = utils.darken_color("#ffffff", 0.25)
+        self.assertEqual(result, "#bfbfbf")
+
+    def test_lighten_color(self):
+        """> Lighten #000000 by 0.25."""
+        result = utils.lighten_color("#000000", 0.25)
+        self.assertEqual(result, "#3f3f3f")
+
+
+if __name__ == "__main__":
+    unittest.main()
