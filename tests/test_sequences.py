694948a271d7249ece384fb2da1da5debde41414
Tran Khanh <tran_khanh@outlook.com>
2021-05-18 15:43:54 +0700

    implement test for Sequence

---
+++ tests/test_sequences.py
@@ -0,0 +1,55 @@
+"""Test sequence functions."""
+import unittest
+import platform
+
+from pycolor import sequences
+from pycolor import utils
+
+# Import colors.
+COLORS = utils.read_file_json("test_files/test_file.json")
+
+
+class Testsequences(unittest.TestCase):
+    """Test the sequences functions."""
+
+    def test_set_special(self):
+        """> Create special escape sequence."""
+        alpha = "100"
+        result = sequences.set_special(11,
+                                       COLORS["special"]["background"],
+                                       "h", alpha)
+
+        if platform.uname()[0] == "Darwin":
+            self.assertEqual(result, "\033]Ph1F211E\033\\")
+        else:
+            self.assertEqual(result, "\033]11;#1F211E\033\\")
+
+    def test_set_special_alpha(self):
+        """> Create special escape sequence with alpha."""
+        alpha = "99"
+        result = sequences.set_special(11,
+                                       COLORS["special"]["background"],
+                                       "h", alpha)
+
+        if platform.uname()[0] == "Darwin":
+            self.assertEqual(result, "\033]Ph1F211E\033\\")
+        else:
+            self.assertEqual(result, "\033]11;[99]#1F211E\033\\")
+
+    def test_set_color(self):
+        """> Create color escape sequence."""
+        result = sequences.set_color(11, COLORS["colors"]["color0"])
+
+        if platform.uname()[0] == "Darwin":
+            self.assertEqual(result, "\033]Pb1F211E\033\\")
+        else:
+            self.assertEqual(result, "\033]4;11;#1F211E\033\\")
+
+    def test_set_iterm_tab_color(self):
+        """> Create iterm tab color sequences"""
+        result = sequences.set_iterm_tab_color(COLORS["special"]["background"])
+        self.assertEqual(len(result), 84)
+
+
+if __name__ == "__main__":
+    unittest.main()
