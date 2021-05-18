508c0e6268fce72c82c5716b532c65bcf6319413
Tran Khanh <tran_khanh@outlook.com>
2021-05-18 14:58:59 +0700

    implement test

--- tests/test_util.py
+++ tests/test_util.py
@@ -15,3 +15,25 @@ class TestUtil(unittest.TestCase):
         """> Read colors from a file."""
         result = utils.read_file("test_files/test_file")
         self.assertEqual(result[0], "/home/trkhanh/Pictures/Wallpapers/1.jpg")
+
+    def test_read_file_start(self):
+        """> Read colors from a file."""
+        result = utils.read_file_json("test_files/test_file.json")
+        self.assertEqual(result["colors"]["color0"], "#1F211E")
+
+    def test_read_file_end(self):
+        """> Read colors from a file."""
+        result = utils.read_file_json("test_files/test_file.json")
+        self.assertEqual(result["colors"]["color15"], "#F5F1F4")
+
+    def test_read_wallpaper(self):
+        """> Read wallpaper from json file."""
+        result = utils.read_file_json("test_files/test_file.json")
+        self.assertEqual(result["wallpaper"], "5.png")
+
+    def test_save_file(self):
+        """> Save colors to a file."""
+        tmp_file = "/tmp/test_file"
+        utils.save_file("Hello, world", tmp_file)
+        result = os.path.isfile(tmp_file)
+        self.assertTrue(result)
